1b15e2f1c8852ca2104587f99b1c167083f239c4
Alecsandra <alecsandra.petruescu@gmail.com>
2016-08-23 23:50:45 +0300

    updated comment

--- app/src/main/java/com/example/alecsandra/funfacts/ColorWheel.java
+++ app/src/main/java/com/example/alecsandra/funfacts/ColorWheel.java
@@ -11,12 +11,11 @@ public class ColorWheel {
 
     // Methods - Actions the object can take
     public void setColor(TypedArray mColorWheel) {
-        //Randomly select a fact
+        //Randomly select a color
         Random randomGenerator = new Random();
         int randomNumber = randomGenerator.nextInt(mColorWheel.length());
         mColor = mColorWheel.getColor(randomNumber, 0);
     }
-
 
     public int getColor(){
         return mColor;
